fddf5a8a7a0021e9e3127cbb661d39bbd17865cb
Harsh Pithadiya <47056249+jarvis3439@users.noreply.github.com>
2020-08-14 23:35:39 -0700

    Created Project domain and projectRepository interface

---
+++ ppm-restapi-advanced/ppm-restapi-advanced/src/main/java/com/jarvis/ppm/PpmRestapiAdvancedApplication.java
@@ -0,0 +1,14 @@
+package com.jarvis.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmRestapiAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmRestapiAdvancedApplication.class, args);
+		System.out.println("Application Started...");
+	}
+
+}
---
+++ ppm-restapi-advanced/ppm-restapi-advanced/src/main/java/com/jarvis/ppm/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.jarvis.ppm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jarvis.ppm.domain.Project;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
