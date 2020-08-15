a620967e512fee7ac12ccfdab420645a3eb3b871
Harsh Pithadiya <47056249+jarvis3439@users.noreply.github.com>
2020-08-15 07:00:48 -0700

    Created ErrorService for returning better error object

--- ppm-restapi-advanced/ppm-restapi-advanced/src/main/java/com/jarvis/ppm/controller/ProjectController.java
+++ ppm-restapi-advanced/ppm-restapi-advanced/src/main/java/com/jarvis/ppm/controller/ProjectController.java
@@ -1,14 +1,18 @@
 package com.jarvis.ppm.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jarvis.ppm.domain.Project;
+import com.jarvis.ppm.service.ErrorService;
 import com.jarvis.ppm.service.ProjectService;
 
 @RestController
@@ -17,10 +21,18 @@ public class ProjectController {
 
 	@Autowired
 	private ProjectService projectService;
-	
+
+	@Autowired
+	private ErrorService errorService;
+
 	// create project request
 	@PostMapping("")
-	public ResponseEntity<Project> createProject(@RequestBody Project project) {
+	public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
+		ResponseEntity<?> error = errorService.errorValidation(result);
+		if (error != null) {
+			return error;
+		}
+
 		projectService.createOrUpdate(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
