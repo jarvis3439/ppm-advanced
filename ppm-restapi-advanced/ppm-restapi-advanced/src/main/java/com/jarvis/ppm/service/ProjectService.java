86fd039cb33645cf781b919876b74f1c276c5d87
Harsh Pithadiya <47056249+jarvis3439@users.noreply.github.com>
2020-08-17 05:57:00 -0700

    Created CustomException for Unique ProjectIdentifier.

--- ppm-restapi-advanced/ppm-restapi-advanced/src/main/java/com/jarvis/ppm/service/ProjectService.java
+++ ppm-restapi-advanced/ppm-restapi-advanced/src/main/java/com/jarvis/ppm/service/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jarvis.ppm.domain.Project;
+import com.jarvis.ppm.exceptions.ProjectIdException;
 import com.jarvis.ppm.repository.ProjectRepository;
 
 @Service
@@ -11,9 +12,14 @@ public class ProjectService {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	// Create or Update Project
 	public Project createOrUpdate(Project project) {
-		return projectRepository.save(project);
+		String identifier = project.getProjectIdentifier().toUpperCase();
+		try {
+			return projectRepository.save(project);
+		} catch (Exception ex) {
+			throw new ProjectIdException("Project ID '" + identifier + "' already exist.");
+		}
 	}
 }
