08ca0611dfad5786f793d7492a1692338363df4b
Harsh Pithadiya <47056249+jarvis3439@users.noreply.github.com>
2020-08-23 22:47:50 -0700

    Created Header and ProjectItem component structure

--- ppm-advanced-react-redux/src/components/Dashboard.js
+++ ppm-advanced-react-redux/src/components/Dashboard.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
         <h1>Welcome to Dashboard</h1>
+        <ProjectItem />
+        <ProjectItem />
       </div>
     );
   }
---
+++ ppm-advanced-react-redux/src/components/Project/ProjectItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ProjectItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Project items</h1>
+      </div>
+    );
+  }
+}
+export default ProjectItem;
